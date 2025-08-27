ad5bdf4fc8cf71ba6f7fc0b6b414fccee1180c83
shila-96 <chepshila96@gmail.com>
2025-08-27 11:52:45 +0300

    Deleted unwanted file(s)

---
+++ lists.py
@@ -0,0 +1,13 @@
+my_list =[]
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(1,15)
+my_list.extend([50,60,70])
+my_list.pop()
+my_list.sort()
+
+print("index of 30:", my_list.index(30))
+print(my_list)
